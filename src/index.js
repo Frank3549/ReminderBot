476d379447a7933289a4a8723eb56fd96206cd7a
Frank3549 <fbautista@middlebury.edu>
2024-03-25 05:50:07 -0700

    linter ran

--- src/index.js
+++ src/index.js
@@ -1,40 +1,41 @@
 require('dotenv').config();
-const {Client, IntentsBitField}  = require('discord.js');
+const { Client, IntentsBitField } = require('discord.js');
 
 // Example in storedTriggerWordsExample.json
-const reactionSpecification = require("./storedTriggerWords.json");
+const reactionSpecification = require('./storedTriggerWords.json');
 
 const TIMERSECONDS = 0.75;
 
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ]
+	intents: [
+		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.GuildMembers,
+		IntentsBitField.Flags.GuildMessages,
+		IntentsBitField.Flags.MessageContent,
+	],
 
 });
 
 client.on('ready', (c) => {
-    console.log(`The ${c.user.tag} is online.`)
+	console.log(`The ${c.user.tag} is online.`);
 });
 
 client.on('messageCreate', (message) => {
-    if(message.author.bot){
-        return;
-    }
-    if(message.content){
-        console.log(message);
-    }
+	if (message.author.bot) {
+		return;
+	}
+	if (message.content) {
+		console.log(message);
+	}
 
-    triggerWords(message, reactionSpecification);
+	triggerWords(message, reactionSpecification);
 
-    
+
 });
 
-client.login(process.env.TOKEN); // Replace with your own token
+// Replace with your own token
+client.login(process.env.TOKEN);
 
 
 /*
@@ -49,10 +50,10 @@ client.login(process.env.TOKEN); // Replace with your own token
 */
 
 function triggerWords(messageObject, reactionSpecifications) {
-    // not efficient. Could use a set, but that would increase complexity of reactionSpecifications.
-    reactionSpecifications.forEach(obj => {
-        reactIfWord(messageObject, obj.wordToReactOn, obj.reactEmoji, obj.temporaryReact);
-    });
+	// not efficient. Could use a set, but that would increase complexity of reactionSpecifications.
+	reactionSpecifications.forEach(obj => {
+		reactIfWord(messageObject, obj.wordToReactOn, obj.reactEmoji, obj.temporaryReact);
+	});
 }
 
 /*
@@ -64,19 +65,20 @@ function triggerWords(messageObject, reactionSpecifications) {
     temporaryReact - Boolean if the reaction is temporary.
 */
 
+// convert to milliseconds
 const reactIfWord = (message, wordToReactOn, reactEmoji, temporaryReact) => {
-    if ((message.content.toLowerCase()).includes(wordToReactOn)){
-        const emojiToSend = client.emojis.cache.find(emoji => emoji.name === reactEmoji); 
-        
-        if(temporaryReact){
-            message.react(emojiToSend)
-            .then(reaction => {
-                setTimeout(() => (reaction.remove()), TIMERSECONDS * 1000) // convert to milliseconds 
-            })
-            .catch(error => console.log(`Couldn't remove the reaction Error: ${error}`))
-        }
-        else{
-            message.react(emojiToSend);
-        }
-    }
-}
+	if ((message.content.toLowerCase()).includes(wordToReactOn)) {
+		const emojiToSend = client.emojis.cache.find(emoji => emoji.name === reactEmoji);
+
+		if (temporaryReact) {
+			message.react(emojiToSend)
+				.then(reaction => {
+					setTimeout(() => (reaction.remove()), TIMERSECONDS * 1000);
+				})
+				.catch(error => console.log(`Couldn't remove the reaction Error: ${error}`));
+		}
+		else {
+			message.react(emojiToSend);
+		}
+	}
+};
